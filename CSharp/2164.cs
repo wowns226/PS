622868b3473a7c183bd06e11c7d3b20f8dd825a3
Riza <wowns226@naver.com>
2022-09-08 08:21:44 +0900

    Create 2164.cs

---
+++ CSharp/2164.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+/***************************************************************************************
+ *   * 링크 : https://www.acmicpc.net/problem/2164
+ ***************************************************************************************/
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+
+            int testCase = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= testCase; i++)
+            {
+                q.Enqueue(i);
+            }
+
+            while (q.Count() > 1)
+            {
+                q.Dequeue();
+
+                q.Enqueue(q.Dequeue());
+            }
+
+            Console.WriteLine(q.Dequeue());
+        }
+    }
+}
