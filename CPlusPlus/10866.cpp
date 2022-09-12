f540353f3522b7955c1a3c56af511ff99ff01727
Riza <wowns226@naver.com>
2022-09-12 09:26:30 +0900

    Update 10866.cpp

--- CPlusPlus/10866.cpp
+++ CPlusPlus/10866.cpp
@@ -3,8 +3,8 @@
 #define FASTIO                             \
     std::ios_base::sync_with_stdio(false); \
     std::cin.tie(0);                       \
-    std::cout.tie(0);
-#define ENDL "\n";
+    std::cout.tie(0)
+#define ENDL "\n"
 
 /***************************************************************************************
  *   * 링크 : https://www.acmicpc.net/problem/10866
@@ -13,7 +13,7 @@
 using namespace std;
 
 int main() {
-    FASTIO
+    FASTIO;
 
     deque<int> dq;
 
