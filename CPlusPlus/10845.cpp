dc3f6b8cc15539b24208278294466131a988accd
Riza <wowns226@naver.com>
2022-09-12 09:26:32 +0900

    Update 10845.cpp

--- CPlusPlus/10845.cpp
+++ CPlusPlus/10845.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 
-#define FASTIO                                                                 \
-    std::ios_base::sync_with_stdio(false);                                     \
-    std::cin.tie(0);                                                           \
-    std::cout.tie(0);
-#define ENDL "\n";
+#define FASTIO                             \
+    std::ios_base::sync_with_stdio(false); \
+    std::cin.tie(0);                       \
+    std::cout.tie(0)
+#define ENDL "\n"
 
 /***************************************************************************************
  *   * 링크 : https://www.acmicpc.net/problem/10845
@@ -12,7 +12,7 @@
 using namespace std;
 
 int main() {
-    FASTIO
+    FASTIO;
 
     queue<int> q;
     int TestCase;
