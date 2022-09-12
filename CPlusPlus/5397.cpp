ca38db7ad2f0839741e5c5e11a89ba0875509a62
Riza <wowns226@naver.com>
2022-09-12 09:26:18 +0900

    Update 5397.cpp

--- CPlusPlus/5397.cpp
+++ CPlusPlus/5397.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 
-#define FASTIO                                                                 \
-    std::ios_base::sync_with_stdio(false);                                     \
-    std::cin.tie(0);                                                           \
-    std::cout.tie(0);
-#define ENDL "\n";
+#define FASTIO                             \
+    std::ios_base::sync_with_stdio(false); \
+    std::cin.tie(0);                       \
+    std::cout.tie(0)
+#define ENDL "\n"
 
 /***************************************************************************************
  *   * 링크 : https://www.acmicpc.net/problem/5397
@@ -12,25 +12,23 @@
 using namespace std;
 
 int main() {
+    FASTIO;
+
     int testCase;
     cin >> testCase;
 
     list<char> L;
     for (int i = 0; i < testCase; i++) {
-
         string s;
         cin >> s;
 
         auto cursor = L.begin();
 
         for (int j = 0; j < s.length(); j++) {
-
             if (s[j] == '<') {
-                if (cursor != L.begin())
-                    cursor--;
+                if (cursor != L.begin()) cursor--;
             } else if (s[j] == '>') {
-                if (cursor != L.end())
-                    cursor++;
+                if (cursor != L.end()) cursor++;
             } else if (s[j] == '-') {
                 if (cursor != L.begin()) {
                     cursor--;
@@ -41,8 +39,7 @@ int main() {
             }
         }
 
-        for (auto c : L)
-            cout << c;
+        for (auto c : L) cout << c;
 
         s.clear();
         L.clear();
