eb76c56e00873ef19fb0ce866958ca4a3ea64286
Riza <wowns226@naver.com>
2022-09-16 19:54:56 +0900

    Update 1926.cpp

--- CPlusPlus/1926.cpp
+++ CPlusPlus/1926.cpp
@@ -4,7 +4,7 @@
     std::ios_base::sync_with_stdio(false); \
     std::cin.tie(0);                       \
     std::cout.tie(0)
-#define ENDL "\n"
+#define ENDL '\n'
 
 /***************************************************************************************
  *   * 링크 : https://www.acmicpc.net/problem/1926
