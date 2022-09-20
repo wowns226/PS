afa622115c868061a974317d95aab9b22d084a22
Riza <wowns226@naver.com>
2022-09-20 09:34:15 +0900

    [BOJ] 15663 N과 M (9)

---
+++ CPlusPlus/15663.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define FASTIO                             \
+    std::ios_base::sync_with_stdio(false); \
+    std::cin.tie(0);                       \
+    std::cout.tie(0)
+#define ENDL "\n"
+
+/***************************************************************************************
+ *   * 링크 : https://www.acmicpc.net/problem/15663
+ ***************************************************************************************/
+
+using namespace std;
+
+const int MAX = 10;
+
+int n, m;
+int arr[MAX];
+bool check[MAX];
+vector<int> v;
+
+void func(int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << ENDL;
+        return;
+    }
+
+    int tmp = 0;
+    for (int i = 0; i < n; i++) {
+        if (!check[i] && v[i] != tmp) {
+            arr[cnt] = v[i];
+            tmp = arr[cnt];
+            check[i] = true;
+            func(cnt + 1);
+            check[i] = false;
+        }
+    }
+}
+
+int main() {
+    FASTIO;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    func(0);
+
+    return 0;
+}
