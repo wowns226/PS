aa503ece7cbca5db04a5ff9a818aace04d1d8f3a
Riza <wowns226@naver.com>
2022-11-08 00:48:53 +0900

    [BOJ] 10828 스택

---
+++ CSharp/10828.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+/***************************************************************************************
+ *   * 링크 : https://www.acmicpc.net/problem/10828
+ ***************************************************************************************/
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cnt = int.Parse(Console.ReadLine());
+            Stack<int> st = new Stack<int>();
+
+            while (cnt > 0)
+            {
+                string[] cmd = Console.ReadLine().Split(' ');
+                StringBuilder sb = new StringBuilder();
+
+                if (cmd[0] == "push")
+                {
+                    int temp = int.Parse(cmd[1]);
+                    st.Push(temp);
+                    cnt--;
+                    continue;
+                }
+                else if (cmd[0] == "pop")
+                {
+                    if (st.Count == 0)
+                    {
+                        sb.Append("-1");
+                    }
+                    else
+                    {
+                        int temp = st.Pop();
+                        sb.Append(temp);
+                    }
+
+                }
+                else if (cmd[0] == "size")
+                {
+                    sb.Append(st.Count());
+                }
+                else if (cmd[0] == "empty")
+                {
+                    if (st.Count == 0)
+                    {
+                        sb.Append("1");
+                    }
+                    else
+                    {
+                        sb.Append("0");
+                    }
+                }
+                else
+                {
+                    // cmd[0]=="top"
+                    if (st.Count == 0)
+                    {
+                        sb.Append("-1");
+                    }
+                    else
+                    {
+                        int temp = st.Peek();
+                        sb.Append(temp);
+                    }
+                }
+
+                Console.WriteLine(sb);
+                cnt--;
+            }
+        }
+    }
+}
