280d94699f806ba525843c667e2199ec18d0df36
Riza <wowns226@naver.com>
2022-11-11 04:17:50 +0900

    [BOJ] 2753 윤년

---
+++ CSharp/2753.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/2753
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static bool LeapYear(int _year )
+        {
+            if(_year % 4 == 0 && (_year % 100 !=0 || _year% 400 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int year = int.Parse(Console.ReadLine());
+
+            if(LeapYear(year))
+            {
+                sb.Append("1");
+            }
+            else
+            {
+                sb.Append("0");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
