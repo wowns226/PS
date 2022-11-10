52d67ad32b65a63e3364a8f58f2ff79c9ecca6d0
Riza <wowns226@naver.com>
2022-11-11 03:54:02 +0900

    [BOJ] 1935 후위 표기식2

---
+++ CSharp/1935.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+// 링크 : https://www.acmicpc.net/problem/1935
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] alp = new double[26];
+            Stack<double> st = new Stack<double>();
+            int alpNum = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            for (int i = 0; i < alpNum; i++)
+            {
+                alp[i] = float.Parse(Console.ReadLine());
+            }
+
+            foreach (char c in s)
+            {
+                if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    double b = st.Pop();
+                    double a = st.Pop();
+
+                    switch (c)
+                    {
+                        case '+':
+                            st.Push(a + b);
+                            break;
+                        case '-':
+                            st.Push(a - b);
+                            break;
+                        case '*':
+                            st.Push(a * b);
+                            break;
+                        case '/':
+                            st.Push(a / b);
+                            break;
+                    }
+                }
+                else
+                {
+                    st.Push(alp[(int)c - (int)'A']);
+                }
+            }
+
+            Console.WriteLine(string.Format("{0:F2}", st.Pop()));
+        }
+    }
+}
