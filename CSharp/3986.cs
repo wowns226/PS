599047de1acdfe5981df08c7d4af663a0bd96e54
Riza <wowns226@naver.com>
2022-11-11 03:36:38 +0900

    [BOJ] 3986 좋은 단어

---
+++ CSharp/3986.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+// 링크 : https://www.acmicpc.net/problem/3986
+
+namespace BOJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> st = new Stack<char>();
+            int answer = 0;
+            int testCase = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCase; i++)
+            {
+                string s = Console.ReadLine();
+
+                st.Push(s[0]);
+
+                for (int t = 1; t < s.Length; t++)
+                {
+                    if (st.Peek() == s[t])
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        st.Push(s[t]);
+                    }
+                }
+
+                if (st.Count == 0)
+                {
+                    answer++;
+                }
+            }
+
+            Console.WriteLine($"{answer}");
+        }
+    }
+}
