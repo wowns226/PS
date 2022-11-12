e784e00401d9c6bad6c4bb891bd88d8e10829401
Riza <wowns226@naver.com>
2022-11-13 00:21:52 +0900

    [BOJ] 5597 과제 안 내신 분..?

---
+++ CSharp/5597.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/5597
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool[] arr = new bool[31];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 28; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+
+                arr[student] = true;
+            }
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (!arr[i])
+                {
+                    sb.AppendLine(i.ToString());
+                }
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
