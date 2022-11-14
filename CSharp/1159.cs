5d97ffd023b33aec73af3fadd18c5c4bd1ad55cc
Riza <wowns226@naver.com>
2022-11-14 11:00:38 +0900

    [BOJ] 1159 농구 경기

---
+++ CSharp/1159.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+// https://www.acmicpc.net/problem/1159
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int[] playerFirstName = new int[26];
+            int playerNum = int.Parse(Console.ReadLine());
+            char firstName;
+
+            for (int i = 0; i < playerNum; i++)
+            {
+                string s = Console.ReadLine();
+
+                firstName = s[0];
+                int temp = (int)firstName - (int)'a';
+                playerFirstName[temp]++;
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (playerFirstName[i] >= 5)
+                {
+                    firstName = (char)('a' + i);
+                    sb.Append(firstName);
+                }
+            }
+
+            if (sb.Equals(""))
+            {
+                sb.Append("PREDAJA");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
