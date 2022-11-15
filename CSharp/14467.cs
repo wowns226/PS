27afdb3c5edd019ed9e01d49bee0f8d5097cc140
Riza <wowns226@naver.com>
2022-11-15 14:05:31 +0900

    [BOJ] 14467 소가 길을 건너간 이유1

---
+++ CSharp/14467.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/14467
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[11];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = -1;
+            }
+
+            int cowCase = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            for (int i = 0; i < cowCase; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                int cowNum = int.Parse(input[0]);
+                int cowLocation = int.Parse(input[1]);
+
+                if (arr[cowNum - 1] == -1)
+                {
+                    arr[cowNum - 1] = cowLocation;
+                }
+                else
+                {
+                    if (arr[cowNum - 1] == cowLocation)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        arr[cowNum - 1] = cowLocation;
+                        cnt++;
+                    }
+                }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
