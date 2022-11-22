225939099156a17262cdb800999fe11efc38d7c1
Riza <wowns226@naver.com>
2022-11-22 14:18:23 +0900

    [BOJ] 1193 분수찾기

---
+++ CSharp/1193.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/1193
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(ReadLine());
+            int i = 1;
+
+            while (n > i)
+            {
+                n -= i;
+                i++;
+            }
+
+            if (i % 2 == 1)
+            {
+                sb.Append(i + 1 - n);
+                sb.Append('/');
+                sb.Append(n);
+            }
+            else
+            {
+                sb.Append(n);
+                sb.Append('/');
+                sb.Append(i + 1 - n);
+            }
+
+            WriteLine(sb.ToString());
+        }
+    }
+}
