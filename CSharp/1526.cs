2dd4ef0473f595d33185da230e94f2381e9487cc
Riza <wowns226@naver.com>
2022-11-24 02:38:56 +0900

    [BOJ] 1526 가장 큰 금민수

---
+++ CSharp/1526.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/1526
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static bool IsFourOrSeven(int _num)
+        {
+            while(_num != 0)
+            {
+                if(_num % 10 == 4 || _num % 10 == 7)
+                {
+                    _num /= 10;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(ReadLine());
+
+            for(int i = n ; i >= 1 ; i--)
+            {
+                if(IsFourOrSeven(i))
+                {
+                    sb.Append(i);
+                    break;
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
