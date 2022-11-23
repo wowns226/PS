99342874a4f2e1c2fcfe520a986842dacb7f2bbf
Riza <wowns226@naver.com>
2022-11-24 01:32:53 +0900

    [BOJ] 1001 A-B

---
+++ CSharp/1001.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/1001
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var inputs = ReadLine().Split(' ').ToArray();
+
+            int a = int.Parse(inputs[0]);
+            int b = int.Parse(inputs[1]);
+
+            sb.Append(a - b);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
