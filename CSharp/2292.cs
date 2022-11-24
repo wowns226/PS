ef86c16b2b09997405594455f2690f890c9806f9
Riza <wowns226@naver.com>
2022-11-24 16:31:12 +0900

    [BOJ] 2292 벌집

---
+++ CSharp/2292.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/2292
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static int GetMinRoomCount(int _num)
+        {
+            int checkCount = 0;
+            int checkNum = 1;
+
+            while (true)
+            {
+                checkNum += 6 * checkCount;
+
+                if (_num <= checkNum)
+                {
+                    return checkCount + 1;
+                }
+
+                checkCount++;
+            }
+        }
+
+
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(ReadLine());
+
+            sb.Append(GetMinRoomCount(n).ToString());
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
