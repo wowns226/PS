ab25f79352b86435124c24321d5321afa87e0f4e
Riza <wowns226@naver.com>
2022-11-28 11:17:36 +0900

    [BOJ] 26068 치킨댄스를 추는 곰곰이를 본 임스 2

---
+++ CSharp/26068.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/26068
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(ReadLine());
+            int count = 0;
+
+            while(n--> 0)
+            {
+                string input = ReadLine();
+                input = input.Substring(2);
+                int date = int.Parse(input);
+                if(date <= 90)
+                {
+                    count++;
+                }
+            }
+            sb.Append(count.ToString());
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
