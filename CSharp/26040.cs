436c11ce0af5f11e4176a625ee5eaef3fe1acdee
Riza <wowns226@naver.com>
2022-11-30 13:41:44 +0900

    [BOJ] 26040 특정 대문자를 소문자로 바꾸기

---
+++ CSharp/26040.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+// 링크 : https://www.acmicpc.net/problem/26040
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string s = ReadLine();
+            string[] inputs = ReadLine().Split(' ');
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool isSame = false;
+                for (int j = 0; j < inputs.Length; j++)
+                {
+
+                    char A = s[i];
+                    char B = Convert.ToChar(inputs[j]);
+
+                    if (A == B)
+                    {
+                        isSame = true;
+                        break;
+                    }
+                }
+
+                if (isSame)
+                {
+                    sb.Append(Char.ToLower(s[i]).ToString());
+                }
+                else
+                {
+                    sb.Append(s[i].ToString());
+                }
+            }
+
+            WriteLine(sb.ToString());
+        }
+    }
+}
