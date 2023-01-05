79643503fc5e9271a36d280cdede3f341b0e7319
Riza <wowns226@naver.com>
2023-01-05 17:43:48 +0900

    [BOJ] 13699 점화식

---
+++ CSharp/13699.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOJ
+{
+    internal class MainApp
+    {
+        static long[] dp = new long[36];
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            dp[0] = 1;
+            for (int i = 1; i < 36; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    dp[i] += dp[j] * dp[i - j - 1];
+                }
+            }
+
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
