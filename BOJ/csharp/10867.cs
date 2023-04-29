d2ed5570945d2699d56794717fa3630d18434a70
Riza <wowns226@naver.com>
2023-04-29 20:47:08 +0900

    Create 10867.cs

---
+++ BOJ/csharp/10867.cs
@@ -0,0 +1,24 @@
+﻿namespace BOJ
+{
+    class No_10867
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            HashSet<int> hashSet = new HashSet<int>();
+
+            int n = int.Parse(input.ReadLine());
+
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            for(int i = 0 ; i < inputs.Length ; i++)
+                hashSet.Add(inputs[i]);
+            
+            var sorted = hashSet.OrderBy(x => x).ToList();
+
+            foreach(int i in sorted)
+                output.Write($"{i} ");
+        }
+    }
+}
