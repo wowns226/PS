eaa100361de1b65977bf6e7a4d752cf3d2c08ca3
Riza <wowns226@naver.com>
2023-04-29 20:47:11 +0900

    Create 1541.cs

---
+++ BOJ/csharp/1541.cs
@@ -0,0 +1,59 @@
+﻿namespace BOJ
+{
+    class No_1541
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string s = input.ReadLine();
+
+            int answer = 0;
+            string num = string.Empty;
+            bool isMinus = false;
+
+            for(int i = 0 ; i <= s.Length ; i++)
+            {
+                if(i == s.Length)
+                {
+                    if(isMinus)
+                    {
+                        answer -= int.Parse(num);
+                        num = string.Empty;
+                    }
+                    else
+                    {
+                        answer += int.Parse(num);
+                        num = string.Empty;
+                    }
+
+                    break;
+                }
+                
+                if(s[i] == '-' || s[i] == '+')
+                {
+                    if(isMinus)
+                    {
+                        answer -= int.Parse(num);
+                        num = string.Empty;
+                    }
+                    else
+                    {
+                        answer += int.Parse(num);
+                        num = string.Empty;
+                    }
+                }
+                else
+                {
+                    num += s[i];
+                }
+
+                if(s[i]== '-')
+                    isMinus = true;
+            }
+
+            output.Write(answer);
+        }
+    }
+}
