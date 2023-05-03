b76b3e9528b37791c8f03371147e9fa9eb1b865f
Riza <wowns226@naver.com>
2023-05-03 15:12:14 +0900

    Create 2847.cs

---
+++ BOJ/csharp/2847.cs
@@ -0,0 +1,30 @@
+﻿namespace BOJ
+{
+    class No_2847
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = int.Parse(input.ReadLine());
+
+            int[] arr = new int[N];
+
+            for(int i=0 ;i<N ;i++)
+                arr[i] = int.Parse(input.ReadLine());
+
+            int answer = 0;
+            for(int i=N-1 ;i>0 ;i--)
+            {
+                while(arr[i - 1] >= arr[i])
+                {
+                    arr[i - 1]--;
+                    answer++;
+                }
+            }
+
+            output.Write(answer);
+        }
+    }
+}
