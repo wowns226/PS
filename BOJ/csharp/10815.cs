919c7d3308fca7bb7c02fd714293cd64037347a2
Riza <wowns226@naver.com>
2023-05-03 15:12:07 +0900

    Create 10815.cs

---
+++ BOJ/csharp/10815.cs
@@ -0,0 +1,43 @@
+﻿namespace BOJ
+{
+    class No_10815
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(input.ReadLine());
+            int[] cards = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+            Array.Sort(cards);
+
+            int m = int.Parse(input.ReadLine());
+            int[] answer = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+            for(int i = 0 ; i < m ; i++)
+                output.Write($"{BinarySearch(cards, answer[i])} ");
+        }
+
+        static int BinarySearch(int[] arr, int num)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while(start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                if(arr[mid] == num)
+                    return 1;
+
+                if(arr[mid] < num)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+
+            return 0;
+        }
+    }
+}
