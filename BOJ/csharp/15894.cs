aa0efae4ab13c6e2476e69fd5a709cd13a13a067
Riza <wowns226@naver.com>
2023-05-08 11:27:17 +0900

    Create 15894.cs

---
+++ BOJ/csharp/15894.cs
@@ -0,0 +1,15 @@
+﻿namespace BOJ
+{
+    class No_15894
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            long n = int.Parse(input.ReadLine());
+
+            output.Write(n * 4);
+        }
+    }
+}
