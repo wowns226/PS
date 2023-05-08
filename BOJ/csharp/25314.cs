18dfc00757665f84d5cb9c461ad9adf84d0ec008
Riza <wowns226@naver.com>
2023-05-08 14:42:54 +0900

    Create 25314.cs

---
+++ BOJ/csharp/25314.cs
@@ -0,0 +1,21 @@
+﻿namespace BOJ
+{
+    class No_25314
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(input.ReadLine());
+
+            int count = n / 4;
+            while(count-->0)
+            {
+                output.Write("long ");
+            }
+
+            output.Write("int");
+        }
+    }
+}
