56f48c3943d7ceea07861759dd4324f0c8deb532
Riza <wowns226@naver.com>
2023-05-08 11:27:32 +0900

    Create 6359.cs

---
+++ BOJ/csharp/6359.cs
@@ -0,0 +1,39 @@
+﻿namespace BOJ
+{
+    class No_6359
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int testcase = int.Parse(input.ReadLine());
+
+            while(testcase-->0)
+            {
+                int n = int.Parse(input.ReadLine());
+                bool[] rooms = new bool[n + 1];
+
+                for(int i=1 ;i<n+1 ;i++)
+                {
+                    int idx = i;
+                    while(idx <n+1)
+                    {
+                        rooms[idx] = !rooms[idx];
+                        idx += i;
+                    }
+                }
+
+                int count = 0;
+                for(int i=1 ;i<n+1 ;i++)
+                {
+                    if(!rooms[i]) continue;
+
+                    count++;
+                }
+
+                output.WriteLine(count);
+            }
+        }
+    }
+}
