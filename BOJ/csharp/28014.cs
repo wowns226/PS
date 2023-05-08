a97cda504a63ddaf1499952a45de6e76f11fbbad
Riza <wowns226@naver.com>
2023-05-08 11:27:23 +0900

    Create 28014.cs

---
+++ BOJ/csharp/28014.cs
@@ -0,0 +1,24 @@
+﻿namespace BOJ
+{
+    class No_28014
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(input.ReadLine());
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+            int answer = 1;
+            for(int i=1 ; i < inputs.Length ;i++)
+            {
+                if(inputs[i] < inputs[i - 1]) continue;
+
+                answer++;
+            }
+
+            output.Write(answer);
+        }
+    }
+}
