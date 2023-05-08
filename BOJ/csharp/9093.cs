d3b8dfcd498846ee26eb074aa76b59271110ea12
Riza <wowns226@naver.com>
2023-05-08 11:27:33 +0900

    Create 9093.cs

---
+++ BOJ/csharp/9093.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BOJ
+{
+    class No_9093
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int testCase = int.Parse(input.ReadLine());
+
+            while(testCase-->0)
+            {
+                string[] inputs = input.ReadLine().Split();
+
+                Queue<string> q = new Queue<string>();
+                Stack<char> st = new Stack<char>();
+
+                for(int i=0 ;i<inputs.Length; i++)
+                    q.Enqueue(inputs[i]);
+
+                while(q.Count > 0)
+                {
+                    var cur = q.Dequeue();
+                    var count = cur.Length;
+
+                    for(int i = 0 ; i < count ; i++)
+                        st.Push(cur[i]);
+
+                    for(int i = 0 ; i < count ; i++)
+                        output.Write(st.Pop());
+
+                    output.Write(' ');
+                }
+
+                output.WriteLine();
+            }
+        }
+    }
+}
