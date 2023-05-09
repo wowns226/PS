2321bf0d7fb0c1d795eb2ad3b97f727814124b2c
Riza <wowns226@naver.com>
2023-05-09 11:06:18 +0900

    Create 2003.cs

---
+++ BOJ/csharp/2003.cs
@@ -0,0 +1,43 @@
+﻿namespace BOJ
+{
+    class No_2003
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            int n = inputs[0];
+            int m = inputs[1];
+
+            int[] arr = new int[10001];
+            inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            for(int i = 0 ; i < n ; i++)
+                arr[i] = inputs[i];
+
+            int cnt = 0;
+            int sum = 0;
+            int st = 0;
+            int en = 0;
+            while(en <= n)
+            {
+                if(sum < m)
+                {
+                    sum += arr[en];
+                    en++;
+                }
+                else
+                {
+                    if(sum == m)
+                        cnt++;
+
+                    sum -= arr[st];
+                    st++;
+                }
+            }
+
+            output.Write(cnt);
+        }
+    }
+}
