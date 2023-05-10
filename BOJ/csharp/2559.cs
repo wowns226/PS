209bbabb67449baa30c49fb92be19e3ba3f26511
Riza <wowns226@naver.com>
2023-05-10 18:44:29 +0900

    Create 2559.cs

---
+++ BOJ/csharp/2559.cs
@@ -0,0 +1,30 @@
+﻿namespace BOJ
+{
+    class No_2559
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            int n = inputs[0];
+            int k = inputs[1];
+
+            inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            int sum = 0;
+            for(int i = 0 ; i < k ; i++)
+                sum += inputs[i];
+
+            int myMax = sum;
+            
+            for(int i=k ;i<n ;i++)
+            {
+                sum += inputs[i] - inputs[i - k];
+                myMax = Math.Max(sum, myMax);
+            }
+
+            output.Write(myMax);
+        }
+    }
+}
