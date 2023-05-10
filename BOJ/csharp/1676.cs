21c3695122c202b4338e28e163a10b7c2811bf34
Riza <wowns226@naver.com>
2023-05-10 18:44:27 +0900

    Create 1676.cs

---
+++ BOJ/csharp/1676.cs
@@ -0,0 +1,15 @@
+﻿namespace BOJ
+{
+    class No_1676
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(input.ReadLine());
+
+            output.Write(n/5 + n/25 + n/125);
+        }
+    }
+}
