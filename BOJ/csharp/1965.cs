9308afdbda809fc875d7f292870ac971e21eda6c
Riza <wowns226@naver.com>
2023-05-12 18:00:43 +0900

    Create 1965.cs

---
+++ BOJ/csharp/1965.cs
@@ -0,0 +1,31 @@
+﻿namespace BOJ
+{
+    class No_1965
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(input.ReadLine());
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+            int[] dp = new int[n];                
+
+            int myMax = int.MinValue;
+            for(int i=0 ;i<n ;i++)
+            {
+                dp[i] = 1;
+                for(int j=0 ;j<i ;j++)
+                {
+                    if(inputs[i] <= inputs[j]) continue;
+                    else dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+
+                myMax = Math.Max(dp[i], myMax);
+            }
+
+            output.Write(myMax);
+        }
+    }
+}
