c1bc4760b390c42486da4fdd276e227d2d1d9bfa
Riza <wowns226@naver.com>
2023-05-15 13:03:10 +0900

    Create 10811.cs

---
+++ BOJ/csharp/10811.cs
@@ -0,0 +1,33 @@
+﻿namespace BOJ
+{
+    class No_10811
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int[] inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            int n = inputs[0];
+            int m = inputs[1];
+
+            int[] arr = new int[n + 1];
+
+            for(int i = 1 ; i <= n ; i++)
+                arr[i] = i;
+
+            while(m-->0)
+            {
+                inputs = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+
+                int i = inputs[0];
+                int j = inputs[1];
+
+                Array.Reverse(arr, i, j - i + 1);
+            }
+
+            for(int i = 1 ; i <= n ; i++)
+                output.Write($"{arr[i]} ");
+        }
+    }
+}
