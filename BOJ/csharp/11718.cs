a74d1d34ac138ebcc18180f2f1a6109fd8da9f8a
Riza <wowns226@naver.com>
2023-05-16 16:53:29 +0900

    Update 11718.cs

--- BOJ/csharp/11718.cs
+++ BOJ/csharp/11718.cs
@@ -1,62 +1,22 @@
 namespace BOJ
 {
-    class No_19583
+    class No_11718
     {
-        static void Main()
+        public static void Main()
         {
             using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
             using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-            string[] inputs = input.ReadLine().Split();
-            string s = inputs[0];
-            string e = inputs[1];
-            string q = inputs[2];
-
-            int[] times = new int[3];
-
-            List<string> checkin = new List<string>();
-            List<string> checkout = new List<string>();
-
-            for(int i = 0 ; i < inputs.Length ; i++)
-            {
-                int[] timeArray = Array.ConvertAll(inputs[i].Split(':'), int.Parse);
-
-                int time = timeArray[0] * 60 + timeArray[1];
-
-                times[i] = time;
-            }
-
+            string answer = "";
             while(true)
             {
-                string temp = input.ReadLine();
+                string s = input.ReadLine();
 
-                if(string.IsNullOrEmpty(temp))
+                if(string.IsNullOrEmpty(s))
                     break;
 
-                inputs = temp.Split();
-
-
-                int[] timeArray = Array.ConvertAll(inputs[0].Split(':'), int.Parse);
-
-                int time = timeArray[0] * 60 + timeArray[1];
-
-                string name = inputs[1];
-
-                if(time <= times[0])
-                {
-                    checkin.Add(name);
-                }
-                else if(times[1] <= time && time <= times[2])
-                {
-                    checkout.Add(name);
-                }
+                output.WriteLine(s);
             }
-
-            List<string> list = new List<string>();
-
-            list = checkin.Intersect(checkout).ToList();
-
-            output.Write(list.Count);
         }
     }
 }
