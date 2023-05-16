6d789795fa5c0cd5930fe72b6fc34a8c42e604bc
Riza <wowns226@naver.com>
2023-05-16 16:52:55 +0900

    Create 27866.cs

---
+++ BOJ/csharp/27866.cs
@@ -0,0 +1,16 @@
+﻿namespace BOJ
+{
+    class No_27866
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string s = input.ReadLine();
+            int i = int.Parse(input.ReadLine());
+
+            output.Write(s[i - 1]);
+        }
+    }
+}
