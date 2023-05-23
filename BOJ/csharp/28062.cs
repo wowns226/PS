fe39a4454261ecedd52d4069573dad26c05a63bf
Riza <wowns226@naver.com>
2023-05-23 18:10:12 +0900

    Create 28062.cs

---
+++ BOJ/csharp/28062.cs
@@ -0,0 +1,33 @@
+﻿namespace BOJ
+{
+    class No_28062
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(input.ReadLine());
+            int[] arr = Array.ConvertAll(input.ReadLine().Split(), int.Parse);
+            List<int> list = new List<int>();
+
+            int sum = 0;
+            for(int i=0 ;i<n ;i++)
+            {
+                if(arr[i] % 2 == 0)
+                    sum += arr[i];
+                else
+                    list.Add(arr[i]);
+            }
+
+            list = list.OrderByDescending(x => x).ToList();
+
+            for(int i=0 ;i<(list.Count/2)*2 ;i++)
+            {
+                sum += list[i];
+            }
+
+            output.Write(sum);
+        }
+    }
+}
