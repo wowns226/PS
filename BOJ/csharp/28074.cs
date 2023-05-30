6b281a5a7acad70db922986d2749dd8c3abc1dd7
Riza <wowns226@naver.com>
2023-05-30 13:59:54 +0900

    Create 28074.cs

---
+++ BOJ/csharp/28074.cs
@@ -0,0 +1,23 @@
+﻿namespace BOJ
+{
+    class No_28074
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            bool[] arr = new bool[27];
+
+            string s = input.ReadLine();
+
+            for(int i = 0 ; i < s.Length ; i++)
+                arr[s[i] - 'A'] = true;
+
+            if(arr['M' - 'A'] && arr['O' - 'A'] && arr['B' - 'A'] && arr['I' - 'A'] && arr['S' - 'A'])
+                output.Write("YES");
+            else
+                output.Write("NO");
+        }
+    }
+}
