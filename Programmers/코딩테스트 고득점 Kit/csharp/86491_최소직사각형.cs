18b0d5eb1db730cd6f6c81e3cbbe064fb48da3a9
Riza <wowns226@naver.com>
2023-06-02 17:17:58 +0900

    Create 86491_최소직사각형.cs

---
+++ Programmers/코딩테스트 고득점 Kit/csharp/86491_최소직사각형.cs
@@ -0,0 +1,43 @@
+namespace cs
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            int[,] sizes = { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } };
+
+            Solution sol = new Solution();
+
+            int answer = sol.solution(sizes);
+
+            Console.Write($"{answer}");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[,] sizes)
+        {
+            int maxWidth = 0;
+            int maxHeight = 0;
+
+            for(int i = 0 ; i < sizes.GetLength(0) ; i++)
+            {
+                int width = sizes[i, 0];
+                int height = sizes[i, 1];
+
+                if(width < height)
+                {
+                    int temp = width;
+                    width = height;
+                    height = temp;
+                }
+
+                maxWidth = Math.Max(maxWidth, width);
+                maxHeight = Math.Max(maxHeight, height);
+            }
+
+            return maxWidth * maxHeight;
+        }
+    }
+}
