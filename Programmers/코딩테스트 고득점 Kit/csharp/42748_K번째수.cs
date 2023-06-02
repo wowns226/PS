61be0c6c52dc1833bcf190590f4d1c3aa3730a42
Riza <wowns226@naver.com>
2023-06-02 17:18:00 +0900

    Create 42748_K번째수.cs

---
+++ Programmers/코딩테스트 고득점 Kit/csharp/42748_K번째수.cs
@@ -0,0 +1,42 @@
+namespace cs
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            int[] array = { 1, 5, 2, 6, 3, 7, 4 };
+            int[,] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+
+            Solution sol = new Solution();
+
+            int[] answer = sol.solution(array, commands);
+
+            foreach(int i in answer)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] array, int[,] commands)
+        {
+            int[] answer = new int[commands.GetLength(0)];
+            List<int> list = new List<int>(array);
+
+            for(int c=0 ;c<commands.GetLength(0) ; c++)
+            {
+                int i = commands[c,0]-1;
+                int j = commands[c,1];
+                int k = commands[c,2]-1;
+
+                var list2 = list.Where((x, idx) => idx >= i && idx < j).OrderBy(x => x).ToList();
+
+                answer[c] = list2[k];
+            }
+
+            return answer;
+        }
+    }
+}
