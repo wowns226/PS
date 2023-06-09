65f6848529b9b1bafd5cb125bb609cb32f027100
Riza <wowns226@naver.com>
2023-06-09 15:42:57 +0900

    Create 43165_타겟넘버.cs

---
+++ Programmers/코딩테스트 고득점 Kit/csharp/43165_타겟넘버.cs
@@ -0,0 +1,45 @@
+namespace cs
+{
+    class MainApp
+    {
+        static int answer = 0;
+
+        static void Main()
+        {
+            Console.WriteLine(TestCase(new int[] { 1, 1, 1, 1, 1 }, 3, 5));
+            Console.WriteLine(TestCase(new int[] { 4, 1, 2, 1 }, 4, 2));
+        }
+
+        static string TestCase(int[] numbers, int target, int returnValue)
+        {
+            int testCaseValue = Solution(numbers, target);
+
+            if(testCaseValue == returnValue)
+                return "성공";
+
+            return "실패";
+        }
+
+        static int Solution(int[] numbers, int target)
+        {
+            int answer = 0;
+
+            DFS(numbers, target, 0, 0, ref answer);
+
+            return answer;
+        }
+
+        static void DFS(int[] numbers, int target, int idx, int number, ref int answer)
+        {
+            if(idx == numbers.Length)
+            {
+                if(number == target) answer++;
+
+                return;
+            }
+
+            DFS(numbers, target, idx + 1, number + numbers[idx], ref answer);
+            DFS(numbers, target, idx + 1, number - numbers[idx], ref answer);
+        }
+    }
+}
