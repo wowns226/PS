41adf14093e02cd69a3ba7c4a78943858a7dd502
Riza <wowns226@naver.com>
2023-08-08 21:33:38 +0900

    Create 15733.cs

---
+++ BOJ/csharp/15733.cs
@@ -0,0 +1,14 @@
+namespace BOJ
+{
+    class No_15733
+    {
+        static void Main()
+        {
+            using StreamReader input = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter output = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            output.WriteLine("I'm Sexy");
+        }
+    }
+}
+
