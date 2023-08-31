242b3546400b6dbf2dae2a46469701cbad2dff7b
Riza <83100394+wowns226@users.noreply.github.com>
2023-08-31 20:14:57 +0900

    [Silver I] Title: 경로 찾기, Time: 76 ms, Memory: 6208 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cs
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cs
@@ -1,69 +1,50 @@
+using System.Text;
+
 namespace BOJ
 {
     class No_11403
     {
         const int MAX = 102;
+        const int INF = 987654321;
 
         static int n;
         static int[,] board = new int[MAX, MAX];
-        static int[,] answer = new int[MAX, MAX];
-        static bool[] visited = new bool[MAX];
-        static Queue<(int, int)> q = new Queue<(int, int)>();
 
         static void Main()
         {
-            int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            n = inputs[0];
+            StringBuilder sb = new StringBuilder();
+            n = int.Parse(Console.ReadLine());
 
             for(int i=0 ;i<n ;i++)
             {
-                inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
                 for(int j=0 ;j<n ;j++)
                 {
-                    board[i, j] = inputs[j];
+                    board[i, j] = inputs[j] == 0 ? INF : inputs[j];
                 }
             }
 
-            for(int i = 0 ; i < n ; i++)
-            {
-                for(int k = 0 ; k < n ; k++)
-                    visited[k] = false;
-
-                for(int j = 0 ; j < n ; j++)
-                {
-                    if (board[i, j] == 1)
-                    {
-                        q.Enqueue((i,j));
-                        answer[i, j] = 1;
-
-                        while(q.Count > 0)
-                        {
-                            var cur = q.Dequeue();
-                            var curFrom = cur.Item1;
-                            var curTo = cur.Item2;
-
-                            for(int k=0 ;k<n ;k++)
-                            {
-                                if(board[curTo, k] == 0) continue;
-                                if(visited[k]) continue;
-
-                                q.Enqueue((curTo, k));
-                                visited[k] = true;
-                                answer[i, k] = 1;
-                            }
-                        }
-                    }
-                }
-            }
+            Floyd();
 
             for(int i = 0 ; i < n ; i++)
             {
                 for(int j = 0 ; j < n ; j++)
                 {
-                    Console.Write($"{answer[i,j]} ");
+                    sb.Append(board[i, j] == INF ? "0 " : "1 ");
                 }
-                Console.WriteLine();
+                sb.Append('\n');
             }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void Floyd()
+        {
+            for(int k = 0 ; k < n ; k++)
+                for(int x = 0 ; x < n ; x++)
+                    for(int y = 0 ; y < n ; y++)
+                        if(board[x, y] > board[x, k] + board[k, y])
+                            board[x, y] = board[x, k] + board[k, y];
         }
     }
 }
