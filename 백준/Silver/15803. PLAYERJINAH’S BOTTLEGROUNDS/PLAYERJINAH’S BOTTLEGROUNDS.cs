91dd59c8ff5ef3f1ccd607bc58d8f865e4d8cbef
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2023-11-27 19:27:59 +0900

    [Silver V] Title: PLAYERJINAH’S BOTTLEGROUNDS, Time: 80 ms, Memory: 6012 KB -BaekjoonHub

---
+++ 백준/Silver/15803. PLAYERJINAH’S BOTTLEGROUNDS/PLAYERJINAH’S BOTTLEGROUNDS.cs
@@ -0,0 +1,34 @@
+namespace BOJ_2527
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<(int, int)> list = new List<(int, int)>();
+            
+            for (int i = 0; i < 3; i++)
+            {
+                int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+                list.Add((inputs[0], inputs[1]));
+            }
+
+            list.Sort();
+
+            int dx1 = list[2].Item1 - list[1].Item1;
+            int dy1 = list[2].Item2 - list[1].Item2;
+
+            int dx2 = list[1].Item1 - list[0].Item1;
+            int dy2 = list[1].Item2 - list[0].Item2;
+
+            if (dy1 * dx2 == dy2 * dx1)
+            {
+                Console.WriteLine("WHERE IS MY CHICKEN?");
+            }
+            else
+            {
+                Console.WriteLine("WINNER WINNER CHICKEN DINNER!");
+            }
+        }
+    }
+}
