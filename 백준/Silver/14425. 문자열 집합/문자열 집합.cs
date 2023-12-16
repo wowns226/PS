51cf8957ce18e995644b065a57202b978221ceb4
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2023-12-16 15:08:12 +0900

    [Silver IV] Title: 문자열 집합, Time: 236 ms, Memory: 29696 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cs
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cs
@@ -4,35 +4,27 @@ namespace BOJ_14425
     {
         static void Main()
         {
-            using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
-            using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-
-            int[] inputs = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
             int n = inputs[0];
             int m = inputs[1];
 
-            Dictionary<string, bool> dict = new Dictionary<string, bool>();
-            List<string> list = new List<string>();
+            HashSet<string> hashSet = new HashSet<string>();
 
             for (int i = 0; i < n; i++)
             {
-                dict.TryAdd(sr.ReadLine(), true);
+                hashSet.Add(Console.ReadLine());
             }
 
             int answer = 0;
             for (int i = 0; i < m; i++)
             {
-                if (dict.ContainsKey(sr.ReadLine()))
+                if (hashSet.Contains(Console.ReadLine()))
                 {
                     answer++;
                 }
             }
             
-            sw.Write(answer);
-            
-            sr.Close();
-            sw.Flush();
-            sw.Close();
+            Console.Write(answer);
         }
     }
 }
