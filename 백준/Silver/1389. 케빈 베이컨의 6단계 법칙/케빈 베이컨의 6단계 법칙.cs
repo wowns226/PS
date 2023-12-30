a894ac2dcf02d99d1ddfe7cb76eea23ec714eb70
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2023-12-31 01:10:28 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 72 ms, Memory: 5848 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cs
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cs
@@ -16,14 +16,14 @@ namespace BOJ
                 int a = inputs[0]-1;
                 int b = inputs[1]-1;
 
-                MakeFriend(a, b, dict);
+                AddFriend(a, b, dict);
             }
 
             int answer = 0;
             int count = int.MaxValue;
             for (int i = 0; i < n; i++)
             {
-                int cnt = SetFriendDistance(i, n, dict);
+                int cnt = CalculateFriendDistance(i, n, dict);
 
                 if (count <= cnt) continue;
                 
@@ -34,11 +34,9 @@ namespace BOJ
             Console.WriteLine(answer);
         }
 
-        static int SetFriendDistance(int task, int n, Dictionary<int, List<int>> dict)
+        static int CalculateFriendDistance(int task, int n, Dictionary<int, List<int>> dict)
         {
             int[] array = new int[n];
-            // 1. dict[task]가 있다면 해당 값들 ++
-            // 3. 2반복
 
             if (dict.ContainsKey(task) != true) return 0;
             
@@ -46,15 +44,13 @@ namespace BOJ
             {
                 array[item] = 1;
 
-                // 2. 반복돌면서 하위 있다면 해당 값들 ++
-                FindChild(task, item, array, dict, 2);
+                UpdateChildDistances(task, item, array, dict, 2);
             }
 
-            // 4. 3번까지 한 기록 리스트의 합을 리턴
             return array.Sum();
         }
 
-        static void FindChild(int parent, int task, int[] array, Dictionary<int, List<int>> dict, int depth)
+        static void UpdateChildDistances(int parent, int task, int[] array, Dictionary<int, List<int>> dict, int depth)
         {
             foreach (var item in dict[task])
             {
@@ -63,26 +59,18 @@ namespace BOJ
                     continue;
                 }
                 
-                // 2-1. 이미 체크한 값이라면 패스
-                if (array[item] != 0)
+                if (array[item] != 0 && array[item] <= depth)
                 {
-                    // 2-2. 더 작다면
-                    if (array[item] > depth)
-                    {
-                        array[item] = depth; 
-                        FindChild(parent, item, array, dict, depth + 1);
-                    }
-                    
                     continue;
                 }
 
                 array[item] = depth;
 
-                FindChild(parent, item, array, dict, depth + 1);
+                UpdateChildDistances(parent, item, array, dict, depth + 1);
             }
         }
 
-        static void MakeFriend(int a, int b, Dictionary<int, List<int>> dict)
+        static void AddFriend(int a, int b, Dictionary<int, List<int>> dict)
         {
             dict.TryAdd(a, new List<int>());
             dict.TryAdd(b, new List<int>());
