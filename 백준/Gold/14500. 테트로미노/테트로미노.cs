e6a860b36a896482043f19edf5bf1de3debc7e17
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2024-01-01 21:52:45 +0900

    [Gold IV] Title: 테트로미노, Time: 316 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cs
+++ 백준/Gold/14500. 테트로미노/테트로미노.cs
@@ -1,80 +1,69 @@
-using System.Text;
+List<int[,]> shapeList = new List<int[,]>()
+{
+    new int[2, 2] { { 1, 1 }, { 1, 1 } },
+    new int[1, 4] { { 1, 1, 1, 1 } },
+    new int[4, 1] { { 1 }, { 1 }, { 1 }, { 1 } },
 
-namespace BOJ
-{    
-    class No_20529
+    new int[2,3]{ { 1, 1, 1 }, { 1, 0, 0 } },
+    new int[2,3]{ { 1, 0, 0 }, { 1, 1, 1 } },
+    new int[2,3]{ { 0, 0, 1 }, { 1, 1, 1 } },
+    new int[2,3]{ { 1, 1, 1 }, { 0, 0, 1 } },
+
+    new int[3,2]{ { 1, 1 }, { 1, 0 }, { 1, 0 } },
+    new int[3,2]{ { 1, 0 }, { 1, 0 }, { 1, 1 } },
+    new int[3,2]{ { 0, 1 }, { 0, 1 }, { 1, 1 } },
+    new int[3,2]{ { 1, 1 }, { 0, 1 }, { 0, 1 } },
+
+    new int[2,3]{ { 1, 1, 0 }, { 0, 1, 1 } },
+    new int[2,3]{ { 0, 1, 1 }, { 1, 1, 0 } },
+    new int[3,2]{ { 1, 0 }, { 1, 1 }, { 0, 1 } },
+    new int[3,2]{ { 0, 1 }, { 1, 1 }, { 1, 0 } },
+
+    new int[2,3]{ { 1, 1, 1 }, { 0, 1, 0 } },
+    new int[2,3]{ { 0, 1, 0 }, { 1, 1, 1 } },
+    new int[3,2]{ { 0, 1 }, { 1, 1 }, { 0, 1 } },
+    new int[3,2]{ { 1, 0 }, { 1, 1 }, { 1, 0 } }
+};
+
+int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = inputs[0];
+int m = inputs[1];
+
+int[,] board = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < m; j++)
     {
-        static void Main()
+        board[i, j] = inputs[j];
+    }
+}
+
+int answer = 0;
+foreach (var item in shapeList)
+{
+    for (int i = 0; i < n - (item.GetLength(0) - 1) ; i++)
+    {
+        for (int j = 0; j < m - (item.GetLength(1) - 1); j++)
         {
-            List<int[,]> shapeList = new List<int[,]>()
+            int sum = 0;
+            for (int x = 0; x < item.GetLength(0); x++)
             {
-                new int[2, 2] { { 1, 1 }, { 1, 1 } },
-                new int[1, 4] { { 1, 1 , 1, 1 } },
-                new int[4, 1] { { 1 }, { 1 } ,{ 1 },{ 1 } },
-
-                new int[2,3]{ { 1, 1, 1 }, { 1, 0, 0 } },
-                new int[2,3]{ { 1, 0, 0 }, { 1, 1, 1 } },
-                new int[2,3]{ { 0, 0, 1 }, { 1, 1, 1 } },
-                new int[2,3]{ { 1, 1, 1 }, { 0, 0, 1 } },
-
-                new int[3,2]{ { 1, 1 }, { 1, 0 }, { 1, 0 } },
-                new int[3,2]{ { 1, 0 }, { 1, 0 }, { 1, 1 } },
-                new int[3,2]{ { 0, 1 }, { 0, 1 }, { 1, 1 } },
-                new int[3,2]{ { 1, 1 }, { 0, 1 }, { 0, 1 } },
-
-                new int[2,3]{ { 1, 1, 0 }, { 0, 1, 1 } },
-                new int[2,3]{ { 0, 1, 1 }, { 1, 1, 0 } },
-                new int[3,2]{ { 1, 0 }, { 1, 1 }, { 0, 1 } },
-                new int[3,2]{ { 0, 1 }, { 1, 1 }, { 1, 0 } },
-
-                new int[2,3]{ { 1, 1, 1 }, { 0, 1, 0 } },
-                new int[2,3]{ { 0, 1, 0 }, { 1, 1, 1 } },
-                new int[3,2]{ { 0, 1 }, { 1, 1 }, { 0, 1 } },
-                new int[3,2]{ { 1, 0 }, { 1, 1 }, { 1, 0 } }
-            };
-
-            int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int n = inputs[0];
-            int m = inputs[1];
-
-            int[,] board = new int[n, m];
-
-            for (int i = 0; i < n; i++)
-            {
-                inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-                for (int j = 0; j < m; j++)
+                for (int y = 0; y < item.GetLength(1); y++)
                 {
-                    board[i, j] = inputs[j];
+                    if(item[x, y] == 0) continue;
+
+                    sum += board[x + i, y + j];
                 }
             }
 
-            int answer = 0;
-            for (int i = 0; i < n; i++)
+            if (answer < sum)
             {
-                for (int j = 0; j < m; j++)
-                {
-                    foreach (int[,] t in shapeList)
-                    {
-                        if (i + t.GetLength(0) > n) continue;
-                        if (j + t.GetLength(1) > m) continue;
-
-                        int sum = 0;
-                        for (int x = 0; x < t.GetLength(0); x++)
-                        {
-                            for (int y = 0; y < t.GetLength(1); y++)
-                            {
-                                if (t[x, y] == 0) continue;
-
-                                sum += board[i + x, j + y];
-                            }
-                        }
-                        
-                        answer = Math.Max(answer, sum);
-                    }
-                }
+                answer = sum;
             }
-
-            Console.WriteLine(answer);
         }
     }
 }
+
+Console.WriteLine(answer);
