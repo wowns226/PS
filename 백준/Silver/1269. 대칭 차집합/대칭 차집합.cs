6b5b2e442791f65c6447f87345c1a1daf3decc50
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2024-01-08 02:09:56 +0900

    [Silver IV] Title: 대칭 차집합, Time: 232 ms, Memory: 55396 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cs
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cs
@@ -5,41 +5,9 @@ namespace BOJ
         static void Main()
         {
             int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int c1 = inputs[0];
-            int c2 = inputs[1];
-
-            HashSet<int> hs1 = new HashSet<int>();
-            HashSet<int> hs2 = new HashSet<int>();
-
-            string[] input = Console.ReadLine().Split();
-            for (int i = 0; i < c1; i++)
-            {
-                hs1.Add(int.Parse(input[i]));
-            }
-            
-            input = Console.ReadLine().Split();
-            for (int i = 0; i < c2; i++)
-            {
-                hs2.Add(int.Parse(input[i]));
-            }
-
-            int answer = 0;
-            HashSet<int> hs = new HashSet<int>();
-            foreach (int item in hs1.Where(item => hs2.Contains(item) == false))
-            {
-                hs.Add(item);
-            }
-
-            answer += hs.Count;
-            hs.Clear();
-            
-            foreach (int item in hs2.Where(item => hs1.Contains(item) == false))
-            {
-                hs.Add(item);
-            }
-            
-            answer += hs.Count;
-
+            var hs1 = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+            var hs2 = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+            int answer = hs1.Count(item => hs2.Contains(item) == false) + hs2.Count(item => hs1.Contains(item) == false);
             Console.WriteLine(answer);
         }
     }
