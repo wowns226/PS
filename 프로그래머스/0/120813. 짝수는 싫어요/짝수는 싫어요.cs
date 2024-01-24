50c772b0fd2638e4c538e77a4ffab3f1bfb4576d
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2024-01-24 21:29:28 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.82 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n) {
+        List<int> answer = new List<int>();
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(i % 2 == 0) continue;
+            
+            answer.Add(i);
+        }
+        
+        return answer.ToArray();
+    }
+}
