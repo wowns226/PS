f92058b2dca876c834994be759f5acd5cc382a91
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2024-01-25 17:37:54 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.29 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(string my_string, int k) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(k-->0)
+        {
+            sb.Append(my_string);
+        }
+        
+        return sb.ToString();
+    }
+}
