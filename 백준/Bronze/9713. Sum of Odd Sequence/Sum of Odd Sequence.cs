05481c880d518d41e6dfa6efffcb74c6386102f5
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2024-02-01 21:04:41 +0900

    [Bronze III] Title: Sum of Odd Sequence, Time: 64 ms, Memory: 5568 KB -BaekjoonHub

---
+++ 백준/Bronze/9713. Sum of Odd Sequence/Sum of Odd Sequence.cs
@@ -0,0 +1,32 @@
+using System.Text;
+
+namespace BOJ
+{
+    static class Input<T>
+    {
+        static public T Get() => ConvertTo(Console.ReadLine());
+        static T ConvertTo(string s) => (T)Convert.ChangeType(s, typeof(T));
+    }
+    
+    class No_10830
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int N = Input<int>.Get();
+            while (N-- > 0)
+            {
+                int number = Input<int>.Get();
+                int answer = 0;
+                for (int i = 1; i <= number; i += 2)
+                {
+                    answer += i;
+                }
+
+                sb.AppendLine($"{answer}");
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
