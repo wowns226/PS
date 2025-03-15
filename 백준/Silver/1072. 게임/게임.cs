ed3e2c7f8e758baf77d5ceb713a1fd09c124e2fa
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2025-03-15 22:42:26 +0900

    [Silver III] Title: 게임, Time: 56 ms, Memory: 5776 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cs
+++ 백준/Silver/1072. 게임/게임.cs
@@ -1,58 +1,54 @@
-namespace BOJ_1072
+using System;
+using System.IO;
+using System.Linq;
+
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main()
+        using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        sw.Write(Solve(sr));
+    }
+
+    private static string Solve(StreamReader sr)
+    {
+        var inputs = sr.ReadLine().Split().Select(long.Parse).ToArray();
+        long totalGames = inputs[0], winGames = inputs[1];
+
+        int currentWinRate = GetWinRate(totalGames, winGames);
+
+        return IsWinRateUnchangeable(currentWinRate) ? "-1" : GetMinimumGamesToIncreaseWinRate(totalGames, winGames, currentWinRate).ToString();
+    }
+
+    private static int GetWinRate(long totalGames, long winGames)
+        => (int)(winGames * 100 / totalGames);
+
+    private static bool IsWinRateUnchangeable(int winRate)
+        => winRate >= 99;
+
+    private static int GetMinimumGamesToIncreaseWinRate(long totalGames, long winGames, int currentWinRate)
+    {
+        long left = 1, right = totalGames;  // 탐색 범위를 totalGames로 줄임 (너무 큰 값 불필요)
+        int newWinRate = currentWinRate;
+
+        while (left <= right)
         {
-            using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
-            using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            long mid = (left + right) / 2;
 
-            var inputs = sr.ReadLine().Split().Select(long.Parse).ToArray();
-
-            long x = inputs[0];
-            long y = inputs[1];
-
-            int probability = GetProbability(x,y);
-
-            if (IsNeverChange(probability))
+            // 승률 비교를 나누기 없이 처리
+            if ((winGames + mid) * 100 >= (totalGames + mid) * (currentWinRate + 1))
             {
-                sw.Write("-1");
+                right = mid - 1;
+                newWinRate = GetWinRate(totalGames + mid, winGames + mid);
             }
             else
             {
-                int answer = BinarySearch(x, y, probability);
-                
-                sw.Write(answer);
+                left = mid + 1;
             }
-            
-            sw.Flush(); sw.Close(); sr.Close();
         }
 
-        static int BinarySearch(long x, long y, int curProbability)
-        {
-            long start = 0;
-            long end = 1_000_000_000;
-
-            while (start <= end)
-            {
-                long mid = (start + end) >> 1;
-                long newProbability = ((y + mid) * 100) / (x + mid);
-
-                if (curProbability >= newProbability)
-                {
-                    start = mid + 1;
-                }
-                else
-                {
-                    end = mid - 1;
-                }
-            }
-
-            return (int)start;
-        }
-
-        static int GetProbability(long x, long y) => (int)(y * 100 / x);
-        static bool IsNeverChange(int probability) => probability > 98;
+        return (int)left;
     }
 }
-
