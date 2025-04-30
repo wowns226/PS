57c273994fb3491cfc841174d32fb2032769a20e
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2025-04-30 12:51:00 +0900

    [Bronze III] Title: 공, Time: 52 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.cs
+++ 백준/Bronze/1547. 공/공.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 
 public class No_1547
@@ -9,38 +8,21 @@ public class No_1547
         using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         using var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-        sw.Write(Solve(sr));
-    }
-
-    private static string Solve(StreamReader sr)
-    {
-        List<int> cupNumbers = new List<int> { 1, 2, 3 };
+        int[] cups = { 1, 2, 3 };
         int m = int.Parse(sr.ReadLine());
 
-        List<(int, int)> swapList = new List<(int, int)>();
         for (int i = 0; i < m; i++)
         {
             var inputs = sr.ReadLine().Split();
-            swapList.Add((int.Parse(inputs[0]), int.Parse(inputs[1])));
+            int a = int.Parse(inputs[0]);
+            int b = int.Parse(inputs[1]);
+
+            int idxA = Array.IndexOf(cups, a);
+            int idxB = Array.IndexOf(cups, b);
+
+            (cups[idxA], cups[idxB]) = (cups[idxB], cups[idxA]);
         }
 
-        Swap(swapList, cupNumbers);
-
-        return cupNumbers[0].ToString();
-    }
-
-    static void Swap(List<(int, int)> swapList, List<int> capNumbers)
-    {
-        foreach((int before, int after) in swapList)
-        {
-            int beforeIdx = 0, afterIdx = 0;
-            for(int i=0;i<capNumbers.Count;i++)
-            {
-                if (capNumbers[i] == before) beforeIdx = i;
-                else if (capNumbers[i] == after) afterIdx = i;
-            }
-            capNumbers[beforeIdx] = after;
-            capNumbers[afterIdx] = before;
-        }
+        Console.WriteLine(cups[0]);
     }
 }
