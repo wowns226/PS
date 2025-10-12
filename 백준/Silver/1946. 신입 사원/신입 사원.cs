22bf4424908d909d24759954f8a9936354f9dc45
LimJaeJun <83100394+wowns226@users.noreply.github.com>
2025-10-12 21:14:59 +0900

    [Silver I] Title: 신입 사원, Time: 1032 ms, Memory: 24416 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cs
+++ 백준/Silver/1946. 신입 사원/신입 사원.cs
@@ -6,49 +6,32 @@ public class BOJ
 {
     // https://www.acmicpc.net/problem/1946
 
-    public class Rank  : IComparable<Rank>
-    {
-        public int FstRank { get; set; }
-        public int SndRank { get; set; }
-        public Rank(int fstRank, int sndRank)
-        {
-            FstRank = fstRank;
-            SndRank = sndRank;
-        }
-        public int CompareTo(Rank? other)
-        {
-            if (other == null) return 1;
-            return FstRank.CompareTo(other.FstRank);
-        }
-    }
-
     public static void Main()
     {
         using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         using var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
         int T = int.Parse(sr.ReadLine());
-        for (int t = 0; t < T; t++)
+        while(T-- > 0)
         {
             int N = int.Parse(sr.ReadLine());
-            var ranks = new List<Rank>(N);
+            var ranks = new List<(int, int)>(N);
             for (int i = 0; i < N; i++)
             {
                 var inputs = sr.ReadLine().Split();
                 int fstRank = int.Parse(inputs[0]);
                 int sndRank = int.Parse(inputs[1]);
-                Rank grade = new Rank(fstRank, sndRank);
-                ranks.Add(grade);
+                ranks.Add((fstRank, sndRank));
             }
-            ranks.Sort();
+            ranks.Sort((a, b) => a.Item1.CompareTo(b.Item1));
             int count = 1;
-            int minSndRank = ranks[0].SndRank;
+            int minRank = ranks[0].Item2;
             for (int i = 1; i < N; i++)
             {
-                if (ranks[i].SndRank < minSndRank)
+                if (ranks[i].Item2 < minRank)
                 {
                     count++;
-                    minSndRank = ranks[i].SndRank;
+                    minRank = ranks[i].Item2;
                 }
             }
             sw.WriteLine(count);
