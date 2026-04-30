a9c32f1c3f9aa2d86527d8a9a310641b96649313
LimJaeJun <wowns226@naver.com>
2026-04-30 19:17:43 +0900

    chore: remove previous LeetCode files

--- 0066-plus-one/0066-plus-one.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        num = int(''.join(map(str, digits)))
-        num += 1
-        return list(map(int, str(num)))
