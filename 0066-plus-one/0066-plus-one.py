63fac65869d74312f5fe958922f29ef4f0096caf
Lim-JaeJun <83100394+wowns226@users.noreply.github.com>
2026-04-30 19:08:24 +0900

    Time: 0 ms (100%), Space: 12.3 MB (54.19%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = int(''.join(map(str, digits)))
+        num += 1
+        return list(map(int, str(num)))
